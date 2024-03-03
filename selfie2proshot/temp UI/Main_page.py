0f3722d2d68eff16b9a9b9979d944b5a8ba2cb2f
alibalapour <alibalapour93.ab@gmail.com>
2024-03-02 17:33:14 -0800

    remove: delete temp UI direcotry

--- selfie2proshot/temp UI/Main_page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-#Task Bar
-st.set_page_config(page_title = "Welcome Home", page_icon = "😮") #layout = "wide"
-
-#Title page
-st.title("Selfie to Headshot generator")
-st.subheader("Need a professional photo but don't have the resources? We're here to help!")
-st.markdown('<a href="/input_page" target="_self">Click on this line to get started!</a>', unsafe_allow_html=True)
